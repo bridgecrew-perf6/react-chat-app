3d92a6a3896a4d561d5853c5a7c3e40d59cabb02
RajrupDasid <rajrupdasid@outlook.com>
2021-12-27 10:13:42 +0530

    development with chat engine started no facing issues - customizing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {ChatEngine} from 'react-chat-engine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+const App=()=>{
+	return(
+
+			<ChatEngine
+			height="100vh"
+				projectID="1153e4f3-e3c2-4bc8-b99e-d3c33528e802"
+				userName="Master"
+				userSecret="masterid"
+				renderChatFeed={(chatAppProps)=><ChatFeed/>}
+			/>
+		);
 }
 
 export default App;
